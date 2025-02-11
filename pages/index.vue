5d9ffbe1c54c8719b283841aed02503300c90edb
BeanieMen <jainarjav886@gmail.com>
2025-02-11 15:45:08 -0500

    feat: improv and auto push to docker

--- pages/index.vue
+++ pages/index.vue
@@ -3,39 +3,39 @@
     <client-only>
       <Background />
     </client-only>
+    <div class="mx-5">
+      <div class="max-w-2xl relative z-20 w-full mx-auto mt-20">
+        <!-- Main -->
+        <div class="flex flex-col items-start mb-20">
+          <span class="text-white text-5xl font-manrope font-bold mb-5">Aarjav Jain</span>
+          <span class="text-[#a0a0a0] text-2xl font-manrope mt-1">Infrastructure enthusiast.</span>
+          <span class="text-[#a0a0a0] text-2xl font-manrope">
+            Working on <a href="https://nexusplay.net" class="animated-underline text-white">@NexusPlay</a>
+          </span>
 
-    <div class="max-w-2xl relative z-20 w-full mx-auto mt-20">
+          <div class="flex items-center gap-4 mt-6">
+            <a href="/resume.pdf" download
+              class="flex items-center gap-2 px-4 py-2 border border-white text-white rounded-lg hover:bg-white hover:text-black transition">
+              <LucideDownload class="w-5 h-5" />
+              <span class="font-semibold">Resume</span>
+            </a>
 
-      <!-- Main -->
-      <div class="flex flex-col items-start mb-20">
-        <span class="text-white text-5xl font-manrope font-bold mb-5">Aarjav Jain</span>
-        <span class="text-[#a0a0a0] text-2xl font-manrope mt-1">Infrastructure enthusiast.</span>
-        <span class="text-[#a0a0a0] text-2xl font-manrope">
-          Working on <a href="https://nexusplay.net" class="animated-underline text-white">@NexusPlay</a>
-        </span>
-
-        <div class="flex items-center gap-4 mt-6">
-          <a href="/resume.pdf" download
-            class="flex items-center gap-2 px-4 py-2 border border-white text-white rounded-lg hover:bg-white hover:text-black transition">
-            <LucideDownload class="w-5 h-5" />
-            <span class="font-semibold">Resume</span>
-          </a>
-
-          <a href="https://github.com/BeanieMen" target="_blank"
-            class="p-2 bg-white rounded-lg hover:opacity-80 transition">
-            <LucideGithub class="w-6 h-6 text-black" />
-          </a>
+            <a href="https://github.com/BeanieMen" target="_blank"
+              class="p-2 bg-white rounded-lg hover:opacity-80 transition">
+              <LucideGithub class="w-6 h-6 text-black" />
+            </a>
+          </div>
         </div>
-      </div>
-      
-      <!-- Skills -->
-      <div>
-        <h2 class="text-white text-5xl font-manrope font-semibold mb-6">Skills</h2>
-        <div class="flex flex-wrap gap-4">
-          <div v-for="skill in skills" :key="skill.name"
-            class="flex items-center gap-1 px-2 py-1 border border-gray-500 rounded-md bg-[#212121] text-white shadow-md">
-            <Icon :name="skill.icon" class="w-5 h-5" />
-            <span class="font-manrope">{{ skill.name}}</span>
+
+        <!-- Skills -->
+        <div>
+          <h2 class="text-white text-5xl font-manrope font-semibold mb-6">Skills</h2>
+          <div class="flex flex-wrap gap-4">
+            <div v-for="skill in skills" :key="skill.name"
+              class="flex items-center gap-1 px-2 py-1 border border-gray-500 rounded-md bg-[#212121] text-white shadow-md">
+              <Icon :name="skill.icon" class="w-5 h-5" />
+              <span class="font-manrope">{{ skill.name }}</span>
+            </div>
           </div>
         </div>
       </div>
